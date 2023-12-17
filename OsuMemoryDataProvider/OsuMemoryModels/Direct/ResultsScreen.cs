b2230767ce1f7a001331f846fe97c607bbeb4a13
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2023-12-17 20:48:09 +0100

    Fix: Score reading on results screen

--- OsuMemoryDataProvider/OsuMemoryModels/Direct/ResultsScreen.cs
+++ OsuMemoryDataProvider/OsuMemoryModels/Direct/ResultsScreen.cs
@@ -6,5 +6,7 @@ namespace OsuMemoryDataProvider.OsuMemoryModels.Direct
     [MemoryAddress("[CurrentRuleset]")]
     public class ResultsScreen : RulesetPlayData
     {
+        [MemoryAddress("[+0x38]+0x78")]
+        public override int Score { get; set; }
     }
 }
